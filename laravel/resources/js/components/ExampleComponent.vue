7b17807f4e7468eb968636c679e16cddd0a01da7
oki2a24 <oki2a24@gmail.com>
2021-09-01 06:51:29 +0900

    chore: Vue.js 3 を追加する

---
+++ laravel/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>ExampleComponent</p>
+    <p>{{ test }}</p>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+    setup() {
+        const test = ref("ref test")
+        return {test}
+    },
+}
+</script>
