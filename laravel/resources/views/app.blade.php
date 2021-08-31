5a5b7c88dfe2957b1c95d3a3436ab17453da406c
oki2a24 <oki2a24@gmail.com>
2021-09-01 08:41:56 +0900

    chore: Vue.js 3 の SPA の形を整える

---
+++ laravel/resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Vue SPA</title>
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <noscript>
+      <strong>We're sorry but this app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+    <div id="app">
+      <example-component/>
+    </div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+  </body>
+</html>
