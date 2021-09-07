85efcbc1c81f36c08f923cacb445265074a18844
oki2a24 <oki2a24@gmail.com>
2021-09-08 06:49:45 +0900

    chore: friendsofphp/php-cs-fixer を追加する

---
+++ laravel/.php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.1.0|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->exclude('bootstrap/cache')
+        ->exclude('node_modules')
+        ->exclude('storage/framework/views')
+        ->exclude('vendor')
+        ->in(__DIR__)
+    )
+;
