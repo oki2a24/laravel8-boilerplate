e5bbf523fcaeb5b3aaac0c61f5df4f1cae2abd7d
oki2a24 <oki2a24@gmail.com>
2022-05-03 11:28:10 +0900

    feat: ダッシュボードページにヘッダーとサイドメニューのサンプルを追加する

--- laravel/resources/js/views/dashboard/Index.vue
+++ laravel/resources/js/views/dashboard/Index.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>dashbord Index.vue</h1>
+  <header-menu />
+
+  <div class="container-fluid">
+    <div class="row">
+      <sidebar-menu />
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div
+          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
+        >
+          <h1>dashbord Index.vue</h1>
+          <router-view />
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script>
+import HeaderMenu from "../HeaderMenu.vue";
+import SidebarMenu from "../SidebarMenu.vue";
+
 export default {
   name: "DashboardIndex",
+  components: { HeaderMenu, SidebarMenu },
   setup() {},
 };
 </script>
