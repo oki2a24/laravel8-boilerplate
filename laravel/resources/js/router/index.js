5cf94a61612e152dbf1161608c6b6ca0ec3a3bea
oki2a24 <oki2a24@gmail.com>
2022-05-04 18:18:13 +0900

    feat: ダッシュボードページ (=ユーザー一覧) を追加する

--- laravel/resources/js/router/index.js
+++ laravel/resources/js/router/index.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/dashboard/Index.vue";
+import HeaderMenuSidebarMenu from "../views/HeaderMenuSidebarMenu.vue";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import UsersIndex from "../views/users/Index.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
-  { path: "/dashboard", name: "Dashboard", component: Dashboard },
   { path: "/login", name: "Login", component: Login },
+  {
+    path: "",
+    component: HeaderMenuSidebarMenu,
+    children: [
+      { path: "/dashboard", name: "Dashboard", component: UsersIndex },
+      { path: "/users", name: "UsersIndex", component: UsersIndex },
+    ],
+  },
 ];
 
 const router = createRouter({
---
+++ laravel/resources/js/views/HeaderMenuSidebarMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <header-menu />
+
+  <div class="container-fluid">
+    <div class="row">
+      <sidebar-menu />
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderMenu from "./HeaderMenu.vue";
+import SidebarMenu from "./SidebarMenu.vue";
+
+export default {
+  name: "HeaderMenuSidebarMenu",
+  components: { HeaderMenu, SidebarMenu },
+  setup() {},
+};
+</script>
