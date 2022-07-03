00d7f7d76a853eec75ed870e69998a563b52c98c
oki2a24 <oki2a24@gmail.com>
2022-07-03 15:04:40 +0900

    feat: プロフィールページに API から取得したデータを表示する

--- laravel/resources/js/views/profile/ShowGeneral.vue
+++ laravel/resources/js/views/profile/ShowGeneral.vue
@@ -1,10 +1,32 @@
 <template>
-  <h1>プロフィール概要</h1>
+  <div class="mb-3 row">
+    <label class="col-sm-2 col-form-label">名前</label>
+    <div class="col-sm-10">
+      <span class="form-control-plaintext">{{ profile.name }}</span>
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label class="col-sm-2 col-form-label">メールアドレス</label>
+    <div class="col-sm-10">
+      <span class="form-control-plaintext">{{ profile.email }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
 export default {
   name: "ProfileShowGeneral",
-  setup() {},
+  setup() {
+    const store = useStore();
+
+    // プロフィール
+    const profile = computed(() => store.getters["profile/data"]);
+    store.dispatch("profile/getIfNeeded");
+
+    return { profile };
+  },
 };
 </script>
--- laravel/resources/js/views/profile/ShowPassword.vue
+++ laravel/resources/js/views/profile/ShowPassword.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>プロフィールパスワード</h1>
+  <div class="mb-3 row">
+    <label class="col-sm-2 col-form-label">パスワード</label>
+    <div class="col-sm-10">
+      <span class="form-control-plaintext">**********</span>
+    </div>
+  </div>
 </template>
 
 <script>
